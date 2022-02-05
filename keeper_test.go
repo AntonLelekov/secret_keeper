4f72ca7003fb81136c08946bbada52dad99a6622
lelekov <lelekov-a@inbox.ru>
2022-02-05 15:22:25 +0300

    add DummyKeeper message

---
+++ keeper_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestDummyKeeperSet(t *testing.T) {
+	keeper := DummyKeeper{mem: make(map[string]string), mu: &sync.Mutex{}}
+	key := "foo"
+	value := "bar"
+	keeper.SET(key, value)
+	savedMsg, _ := keeper.GET(key)
+	if savedMsg != value {
+		t.Error("bad memory value")
+	}
+}
+
+func TestDummyKeeperGet(t *testing.T) {
+	keeper := DummyKeeper{mem: make(map[string]string), mu: &sync.Mutex{}}
+	key := "foo"
+	value := "bar"
+	encryptedValue, _ := encrypt.Encrypt(value)
+	keeper.mem[key] = encryptedValue
+	value_from_get, _ := keeper.GET(key)
+	if value_from_get != value {
+		t.Error("bad value from get", value_from_get, value)
+	}
+}
+
+func TestDummyKeeperClean(t *testing.T) {
+	keeper := DummyKeeper{mem: make(map[string]string), mu: &sync.Mutex{}}
+	key := "foo"
+	value := "bar"
+	keeper.mem[key] = value
+	keeper.Clean(key)
+	_, ok := keeper.mem[key]
+	if ok {
+		t.Error("clean does not work")
+	}
+}
