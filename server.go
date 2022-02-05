6a5fb847d6f1bec6b7459a18f7b5ad0b38f1b715
lelekov <lelekov-a@inbox.ru>
2022-02-05 12:45:36 +0300

    server run, templates

--- server.go
+++ server.go
@@ -1,1 +1,22 @@
-package secret_keeper
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func indexView(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+}
+
+func getRouter() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLFiles("templates/index.html")
+	router.GET("/", indexView)
+	return router
+}
+
+func main() {
+	router := getRouter()
+	router.Run("localhost:9000")
+}
